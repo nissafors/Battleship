b08e8a4a9fb3b99c0c39fcf221f43e1f0e749c17
nissafors <aa.gpost@gmail.com>
2014-11-25 12:28:19 +0100

    Player Class Draft

---
+++ Battleship/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Player
+    {
+        static Square LaunchAtTarget(Square[,] grid, int row, int col)
+        {
+            // IF rutan har redan blivit skjuten på
+            //     return forbidden
+            // ELSE IF träff
+            //     skriv in träff i spelplan
+            //     IF sänkt
+            //         return sänkt
+            //     ELSE
+            //         return träff
+            // ELSE
+            //     skriv in miss i spelplanen
+            //     return miss
+            // END IF
+        }
+
+        private bool isSunk(Square[,] grid, int row, int col)
+        {
+
+        }
+    }
+}
